58dc0983c199fcb860d4834e28a5a8962f20dba6
Araham7 <arahamabeddin7@gmail.com>
2024-12-08 09:08:16 +0530

    Started adding basic redux store with redux-toolkit.

--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,17 @@ import App from './App.jsx';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-redux';
+import store from './Redux/store.js';
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  // 
+  <Provider store={store} > {/* Activating Redux Store in an Entire Application. */}
+  <BrowserRouter> {/* Activating BrowserRouter in an Entire Application. */}
     <App />
     <Toaster />  {/* This way we can enable the `react-hot-toast` throughout the entire app */}
   </BrowserRouter>
+  </Provider>
 )
 
